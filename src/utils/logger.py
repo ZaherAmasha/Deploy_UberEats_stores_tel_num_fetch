a62666dcda34346edb8a9225ceac255091976377
ZaherAmasha <amashaazaherr@gmail.com>
2025-01-24 18:47:40 +0200

    Add logging utils to be used for logging in all files and functionalities

---
+++ src/utils/logger.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+import os
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        # logging.FileHandler("./log_file3.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+logger = logging.getLogger()
+
+log_level = os.getenv("LOG_LEVEL", "INFO")
+logger.setLevel(log_level.upper())  # overrides the above level setting
