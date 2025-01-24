03a25a250684e002d97ffdf1a48e9d98b9d82891
ZaherAmasha <amashaazaherr@gmail.com>
2025-01-24 20:53:01 +0200

    Migrate from Place API to Places API (New): for improved performance, using a single API call instead of two to fetch a phone number, cheaper since it retrieves all the needed data (phone numbers)

--- src/google_places_api.py
+++ src/google_places_api.py
@@ -9,45 +9,36 @@ load_dotenv()
 GOOGLE_PLACES_API_KEY = os.getenv("GOOGLE_PLACES_API_KEY")
 
 
+# Google offers $200 free credits per month then for each 1000 requests the cost will be $32
 def get_phone_number_from_google_maps(store_name, address):
-    # Construct query string
+
     query = f"{store_name}, {address}"
 
-    # Use Place Search to find the place using the name and address
-    search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-    params = {"query": query, "key": GOOGLE_PLACES_API_KEY}
-    response = requests.get(search_url, params=params)
+    search_url = "https://places.googleapis.com/v1/places:searchText"
+    params = {"textQuery": query}
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": GOOGLE_PLACES_API_KEY,
+        "X-Goog-FieldMask": "places.internationalPhoneNumber",
+    }
+    response = requests.post(search_url, params=params, headers=headers)
 
-    with open("placesAPI_response.json", "w") as f:
-        f.write(str(response.text))
+    logger.info(
+        f"This is the response from Places API (New)for query: {query}: {response.text}"
+    )
 
     if response.status_code == 200:
-        places = response.json().get("results", [])
+        places = response.json().get("places", [])
         if places:
-            # Use Place Details to fetch phone number for the first result
-            place_id = places[0].get("place_id")
-            return get_phone_number_from_place_id(place_id)
+            # Extract the formatted phone number from the first result
+            phone_number = places[0].get(
+                "internationalPhoneNumber", "Phone number not available"
+            )
+            return phone_number
         else:
             return "No results found"
     else:
-        return f"Error: {response.status_code}"
-
-
-def get_phone_number_from_place_id(place_id):
-    # Get detailed information for the place
-    details_url = "https://maps.googleapis.com/maps/api/place/details/json"
-    params = {
-        "place_id": place_id,
-        "fields": "formatted_phone_number",
-        "key": GOOGLE_PLACES_API_KEY,
-    }
-    response = requests.get(details_url, params=params)
-
-    if response.status_code == 200:
-        place_details = response.json().get("result", {})
-        return place_details.get("formatted_phone_number", "Phone number not available")
-    else:
-        return f"Error: {response.status_code}"
+        return f"Error: {response.status_code} - {response.text}"
 
 
 # Example usage
