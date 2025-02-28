3979452c0437fc5afc7e9843a579c97830fa0775
ZaherAmasha <amashaazaherr@gmail.com>
2025-02-28 21:15:17 +0200

    Add IAM role policy attachment needed for the Lambda function to perform the required actions in the Dynamodb table

--- deployment/3-dynamodb.tf
+++ deployment/3-dynamodb.tf
@@ -24,3 +24,27 @@ resource "aws_dynamodb_table" "scraped_stores_data"{
     read_capacity = 5
     write_capacity = 5
 }
+
+# Give the Lambda function the permission to access the DynamoDB table
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+    name = "LambdaDynamoDBPolicy"
+    description = "Allows Lambda to read and write to DynamoDB"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Action = [
+                "dynamodb:PutItem",
+                "dynamodb:Query",
+                "dynamodb:BatchWriteItem",
+            ]
+            Resource = aws_dynamodb_table.scraped_stores_data.arn
+        }
+        ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "dynamodb_access" {
+    role       = aws_iam_role.iam_for_lambda.name # This role is defined in the 1-lambda.tf file
+    policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
